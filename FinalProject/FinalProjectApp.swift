47289aa5aac589e8bcd10270ce367395a53a0bc5
Nguyen Duc Huy <huyhdhdhd@gmail.com>
2020-09-22 09:39:35 +0700

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Nguyễn Đức Huy on 9/22/20.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
